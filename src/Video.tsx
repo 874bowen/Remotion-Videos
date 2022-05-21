e53393119ff43e7c21dcf28d016ed2edda8c543e
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-21 11:29:59 +0300

    Main function here with the sequences

--- src/Video.tsx
+++ src/Video.tsx
@@ -15,3 +15,26 @@ export const RemotionVideo: React.FC = () => {
 		</>
 	);
 };
+
+const Main = () => {
+	const {fps, durationInFrames} = useVideoConfig();
+	return (
+		<div style={{backgroundColor: 'white', flexGrow: 1}}>
+			<Sequence from={fps*0.3} durationInFrames={durationInFrames}>
+				<CloudinaryImage />
+			</Sequence>
+			<Sequence from={fps} durationInFrames={durationInFrames}>
+				<Title />
+			</Sequence>
+			<Sequence from={fps*2} durationInFrames={durationInFrames}>
+				<Subtitle />
+			</Sequence>
+		</div>
+		// <div style={{backgroundColor: 'white', flexGrow: 1}}>
+		// 	<h1>Cloudinary</h1>
+		// 	<Img src={cloudinary} alt='logo' className='text-center'/>
+		// 	<p>Unleash the full potential of your digital media
+		// 	Transform images and videos to load faster with no visual degradation, automatically generate image and video variants, and deliver high quality responsive experiences to increase conversions.</p>
+		// </div>
+	);
+}
