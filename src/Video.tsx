116c2a022c04eea4f972b7a83c99658f509bd0c9
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-21 11:29:10 +0300

    Main Composition

--- src/Video.tsx
+++ src/Video.tsx
@@ -5,41 +5,9 @@ export const RemotionVideo: React.FC = () => {
 	return (
 		<>
 			<Composition
-				id="HelloWorld"
-				component={HelloWorld}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1080}
-				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: 'black',
-				}}
-			/>
-			<Composition
-				id="Logo"
-				component={Logo}
-				durationInFrames={200}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Title"
-				component={Title}
-				durationInFrames={100}
-				fps={30}
-				width={1920}
-				height={1080}
-				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: 'black',
-				}}
-			/>
-			<Composition
-				id="Subtitle"
-				component={Subtitle}
-				durationInFrames={100}
+				id="Main"
+				component={Main}
+				durationInFrames={30*3}
 				fps={30}
 				width={1920}
 				height={1080}
